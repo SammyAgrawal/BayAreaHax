15d3eb139b41567f1289e33e978a11f62c4c3227
Julian Dai <julian.dai@gmail.com>
2020-07-18 00:07:14 -0400

    revisions on Flask UI

---
+++ FlaskUI/app/app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template
+import json # py library
+
+file = open('byName.json', 'r')
+dic = json.load(file)
+def getTop(n):
+    result = []
+    sort = sorted(dic.items(), key = lambda rep: rep[1]['Score'], reverse=True)
+    for i in range(n):
+        result.append(sort[i][1])
+    return(result)
+
+app = Flask(__name__)
+
+app.config.from_object('config.Config')
+
+@app.route('/')
+@app.route('/index/')
+def home():
+	return render_template('index.html')
+
+@app.route('/dashboard/')
+def dashboard():
+	return render_template('dashboard.html', data=getTop(5))
+
+@app.route('/choose_campaign/')
+def choose_campaign():
+	return render_template('choose_campaign.html')
+
+
+@app.route('/contacts/')
+def contacts():
+	return render_template('contacts.html')
+
+
+if __name__ == "__main__":
+	app.run(host='localhost', port=app.config['PORT'], debug=app.config['DEBUG'])
